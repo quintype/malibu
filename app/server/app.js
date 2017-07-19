f9e33bd48ae26d0bfbdc1680c8a32776c48a2a65
Anagh Padmanabhan <anagh.parambath@gmail.com>
2017-07-19 17:42:00 +0530

    Adding support for live reloading, and we didn't need any help from @codepodu

--- app/server/app.js
+++ app/server/app.js
@@ -35,7 +35,7 @@ app.all("/news_sitemap.xml", sketchesProxy);
 
 app.get("/foobar", function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send("<html><head><link rel='stylesheet' type='text/css' href='/toddy/assets/app.css'></head><body>Foo<script src='/toddy/assets/app.js' async></script></body></html>");
+  res.send("<html><head><link rel='stylesheet' type='text/css' href='/toddy/assets/app.css'></head><body>Foo<script src='http://localhost:8080/toddy/assets/app.js' async></script></body></html>");
 })
 
 module.exports = function startApp() {
