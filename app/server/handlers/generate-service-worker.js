c0d95437a0f9bad982fbed66b52b6f3ab2923f75
Tejas Dinkar <tejas@gja.in>
2017-07-24 11:29:47 +0530

    Reading service worker contents at compile time

--- app/server/handlers/generate-service-worker.js
+++ app/server/handlers/generate-service-worker.js
@@ -1,12 +1,14 @@
 const {generateRoutes} = require('../routes');
 const {assetPath, readAsset} = require("../asset-helper");
 
+const serviceWorkerContents = readAsset("serviceWorkerHelper.js");
+
 function generateServiceWorker(req, res, {config}) {
   res.header("Content-Type", "application/javascript");
-  res.header("Cache-Control", "public,max-age=3600");
+  res.header("Cache-Control", "public,max-age=300");
   res.render("js/service-worker", {
     routes: generateRoutes(config),
-    serviceWorkerHelper: readAsset("serviceWorkerHelper.js"),
+    serviceWorkerHelper: serviceWorkerContents,
     assetPath: assetPath
   });
   return new Promise((resolve) => resolve());
