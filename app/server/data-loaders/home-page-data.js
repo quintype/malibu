5c224f6a3009b76ff37838d7ef3838d658a3df39
Anagh Padmanabhan <anagh.parambath@gmail.com>
2017-07-26 19:47:27 +0530

    Use collections in homepage.

--- app/server/data-loaders/home-page-data.js
+++ app/server/data-loaders/home-page-data.js
@@ -1,6 +1,8 @@
 const {Story, client} = require("quintype-toddy-libs/server/api-client");
 
 exports.loadHomePageData = function loadHomePageData(){
-  return Story.getStories(client, 'top', {})
-    .then(stories => ({stories: stories.map(story => story.asJson())}));
+  return client.getCollectionBySlug('home', {'item-type': 'story', 'limit': 20})
+    .then(collection => ({
+      stories: collection.items.map(story => story.story)
+    }));
 }
