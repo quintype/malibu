3169eebc9a693f6f7469aafd7d9432eebd51036e
Tejas Dinkar <tejas@gja.in>
2017-08-13 15:55:37 +0530

    True hot reloading

--- app/client/app.js
+++ app/client/app.js
@@ -1,24 +1,27 @@
 import css from '../../app/assets/stylesheets/app.scss';
 
 import { createQtStore } from 'quintype-toddy-libs/store/create-store';
-import { renderIsomorphicComponent, renderBreakingNews, history, navigateToPage, getRouteData } from 'quintype-toddy-libs/client/start';
+import { history, navigateToPage, getRouteData } from 'quintype-toddy-libs/client/start';
 import { NAVIGATE_TO_PAGE } from 'quintype-toddy-libs/store/actions';
 
 global.Promise = global.Promise || require("bluebird");
 global.superagent = require('superagent-promise')(require('superagent'), Promise);
 global.navigateToPage = navigateToPage;
 
-import { pickComponent } from '../isomorphic/pick-component';
-import { BreakingNewsView } from '../isomorphic/components/breaking-news-view';
+import { renderApplication } from './render'
 
 // This is the entry point. Ideally, unused functions will get compiled out
 function startApp() {
   getRouteData(window.location.pathname, {config: true})
     .then((result) => {
       const store = createQtStore(null, result.body);
-      renderIsomorphicComponent('container', store, pickComponent);
-      renderBreakingNews('breaking-news-container', store, BreakingNewsView);
+
+      renderApplication(store);
       history.listen(change => maybeNavigateTo(change.pathname, store));
+
+      if(process.env.NODE_ENV == 'development' && module.hot) {
+        module.hot.accept('./render', () => renderApplication(store));
+      }
     });
 }
 
---
+++ app/client/render.js
@@ -0,0 +1,10 @@
+import { renderIsomorphicComponent, renderBreakingNews, history, navigateToPage, getRouteData } from 'quintype-toddy-libs/client/start';
+
+import { pickComponent } from '../isomorphic/pick-component';
+import { BreakingNewsView } from '../isomorphic/components/breaking-news-view';
+
+// This is a separate file as everything from here on is hot reloaded when the app changes
+export function renderApplication(store) {
+  renderIsomorphicComponent('container', store, pickComponent);
+  renderBreakingNews('breaking-news-container', store, BreakingNewsView);
+}
