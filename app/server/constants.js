a812b1c47bcfebaee0d4206eac60893943c083fb
munirajun <munirajur2014@gmail.com>
2017-09-05 17:30:51 +0530

    Move page type strings to constants file

---
+++ app/server/constants.js
@@ -0,0 +1,11 @@
+const PAGE_TYPE = Object.freeze({
+  HOME_PAGE : "home-page",
+  SECTION_PAGE : "section-page",
+  TAG_PAGE : "tag-page",
+  SEARCH_PAGE : "search-page",
+  STORY_PAGE : "story-page"
+});
+
+module.exports = {
+  PAGE_TYPE : PAGE_TYPE
+};
--- app/server/load-data.js
+++ app/server/load-data.js
@@ -5,6 +5,7 @@ const {loadStoryPageData} = require("./data-loaders/story-page-data");
 const {loadSectionPageData} = require("./data-loaders/section-page-data");
 const {loadTagPageData} = require("./data-loaders/tag-page-data");
 const {loadSearchPageData} = require("./data-loaders/search-page-data");
+const {PAGE_TYPE} = require("./constants");
 
 const WHITELIST_CONFIG_KEYS = ['cdn-image'];
 
@@ -20,17 +21,24 @@ function loadErrorData(error) {
 function loadData(pageType, params, config) {
   function _loadData() {
     switch (pageType) {
-      case "home-page": return loadHomePageData();
-      case "section-page": return loadSectionPageData(params.sectionId, config);
-      case "tag-page": return loadTagPageData(params.tagSlug);
-      case "search-page": return loadSearchPageData(params.searchQuery);
-      case "story-page": return loadStoryPageData(params);
+      case PAGE_TYPE.HOME_PAGE: return loadHomePageData();
+      case PAGE_TYPE.SECTION_PAGE: return loadSectionPageData(params.sectionId, config);
+      case PAGE_TYPE.TAG_PAGE: return loadTagPageData(params.tagSlug);
+      case PAGE_TYPE.SECTION_PAGE: return loadSearchPageData(params.searchQuery);
+      case PAGE_TYPE.STORY_PAGE: return loadStoryPageData(params);
       default: return Promise.resolve({stories: [{headline: "Foobar"}]})
     }
   }
 
   return _loadData()
-    .then((data) => ({pageType: pageType, data: data, config: _.pick(config, WHITELIST_CONFIG_KEYS)}));
+    .then((data) => {
+      return {
+        httpStatusCode : 200,
+        pageType: pageType,
+        data: data,
+        config: _.pick(config, WHITELIST_CONFIG_KEYS)
+      };
+    });    
 }
 
 exports.loadData = loadData;
