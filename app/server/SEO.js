b463644a0fe4fcef49d2fbfe3226c42cfe024c27
munirajun <munirajur2014@gmail.com>
2017-09-06 16:37:15 +0530

    Refactor getMetatags method

--- app/server/SEO.js
+++ app/server/SEO.js
@@ -6,29 +6,35 @@ module.exports = class SEO {
   constructor(config, seoParams) {
     this._config = config;
     this._seoParams = seoParams;
+    this._pageSEOObj = this.getPageSEOObj();
+  }
+
+  getPageSEOObj() {
+    let config = this._config;
+    let seoParams = this._seoParams;
+
+    let pageSEOObj = {};
+    
+    pageSEOObj[PAGE_TYPE.HOME_PAGE] = function () {
+      return new HomeSeo(config);
+    };
+
+    pageSEOObj[PAGE_TYPE.SECTION_PAGE] = function () {
+      return new SectionSeo(config, seoParams.section);
+    };
+
+    pageSEOObj[PAGE_TYPE.STORY_PAGE] = function () {
+      return new StorySeo(config, seoParams.story);
+    };
+
+    return pageSEOObj;
   }
 
   getMetaTags(pageType) {
     let metaTags = [];
-    let config = this._config;
-    let seoParams = this._seoParams;
 
-    let pageSEO = {};
-
-    pageSEO[PAGE_TYPE.HOME_PAGE] = function () {
-      return new HomeSeo(config);
-    };
-
-    pageSEO[PAGE_TYPE.SECTION_PAGE] = function () {
-      return new SectionSeo(config, seoParams.section);
-    };
-
-    pageSEO[PAGE_TYPE.STORY_PAGE] = function () {
-      return new StorySeo(config, seoParams.story);
-    };
-
-    if (pageSEO.hasOwnProperty(pageType)) {
-      return pageSEO[pageType]().getMetaTags();
+    if (this._pageSEOObj.hasOwnProperty(pageType)) {
+      return this._pageSEOObj[pageType]().getMetaTags();
     }
 
     return metaTags;
