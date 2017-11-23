f306962f2a9fe5703c5708fda543cfb19ea7478d
Tejas Dinkar <tejas@gja.in>
2017-11-23 18:19:47 +0530

    Upgrading to toddy libs 1.2

--- app/isomorphic/components/pages/story.jsx
+++ app/isomorphic/components/pages/story.jsx
@@ -3,23 +3,24 @@ const React = require("react");
 const { BlankStory } = require("../story-templates/blank.jsx");
 const { InfiniteStoryBase } = require("quintype-toddy-libs/components/infinite-story-base");
 
-function StoryPageBase({index, story}) {
-  return <BlankStory story={story} index={index} />
+function StoryPageBase({index, story, otherProp}) {
+  // Can switch to a different template based story-template, or only show a spoiler if index > 0
+  return <BlankStory story={story} />
 }
 
 const FIELDS = "id,headline,slug,url,hero-image-s3-key,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,author-name,author-id,sections,story-template,cards";
-function storyPageLoadStories(pageNumber) {
+function storyPageLoadItems(pageNumber) {
   return global.superagent
            .get("/api/v1/stories", {fields: FIELDS, limit:5, offset:5*pageNumber})
-           .then(response => response.body.stories);
+           .then(response => response.body.stories.map(story => ({story: story, otherProp: "value"})));
 }
 
 function StoryPage(props) {
   return <InfiniteStoryBase {...props}
                             render={StoryPageBase}
-                            loadStories={storyPageLoadStories}
-                            onStoryFocus={(story) => console.log(`Story In View: ${story.headline}`)}
-                            onInitialStoryFocus={(story) => console.log(`Do Analytics ${story.headline}`)} />
+                            loadItems={storyPageLoadItems}
+                            onItemFocus={(item) => console.log(`Story In View: ${item.story.headline}`)}
+                            onInitialItemFocus={(item) => console.log(`Do Analytics ${item.story.headline}`)} />
 }
 
 exports.StoryPage = StoryPage;
