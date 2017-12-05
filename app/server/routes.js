162624dd3fbb6099d3ef799574b26da4d3ce676e
munirajun <munirajur2014@gmail.com>
2017-12-05 16:58:27 +0530

    Update routes.js

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,12 +1,14 @@
 import {generateStoryPageRoutes, generateSectionPageRoutes} from "@quintype/framework/server/generate-routes";
+import { PAGE_TYPE } from "../isomorphic/constants";
 
 const STATIC_ROUTES = [
-  {path: "/", pageType: "home-page", exact: true},
-  {path: "/preview/story", pageType: "story-preview-page", exact: true},
-  {path: "/preview/story/:encryptedKey", pageType: "story-public-preview-page", exact: true},
-  {path: "/preview/home", pageType: "home-preview-page", exact: true},
-  {path: "/topic/:tagSlug", pageType: "tag-page", exact: true, skipPWA: true},
-  {path: "/search", pageType: "search-page", exact: true, skipPWA: true},
+  {path: "/", pageType: PAGE_TYPE.HOME_PAGE, exact: true},
+  {path: "/preview/story", pageType: PAGE_TYPE.STORY_PREVIEW, exact: true},
+  {path: "/preview/story/:encryptedKey", pageType: PAGE_TYPE.STORY_PUBLIC_PREVIEW_PAGE, exact: true},
+  {path: "/preview/home", pageType: PAGE_TYPE.HOME_PREVIEW, exact: true},
+  {path: "/topic/:tagSlug", pageType: PAGE_TYPE.TAG_PAGE, exact: true},
+  {path: "/search", pageType: PAGE_TYPE.SEARCH_PAGE, exact: true},
+  {path: "/about-us", pageType: PAGE_TYPE.ABOUT_US, exact: true, skipPWA: true}
 ];
 
 function generateRoutes(config) {
