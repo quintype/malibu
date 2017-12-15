29017f14fa4223c9f67eb6852d9dd8e465e9ab91
Tejas Dinkar <tejas@gja.in>
2017-12-16 01:52:16 +0530

    Upgrading to latest framework

--- app/server/app.js
+++ app/server/app.js
@@ -33,10 +33,10 @@ const STATIC_TAGS = {
 
 const STRUCTURED_DATA = {
   organization: {
-    name:"Quintype",
-    url:"http://www.quintype.com/",
-    logo:"https://quintype.com/logo.png",
-    sameAs:["https://www.facebook.com/quintype","https://twitter.com/quintype_in","https://plus.google.com/+quintype","https://www.youtube.com/user/Quintype"],
+    name: "Quintype",
+    url: "http://www.quintype.com/",
+    logo: "https://quintype.com/logo.png",
+    sameAs: ["https://www.facebook.com/quintype","https://twitter.com/quintype_in","https://plus.google.com/+quintype","https://www.youtube.com/user/Quintype"],
   }
 }
 
