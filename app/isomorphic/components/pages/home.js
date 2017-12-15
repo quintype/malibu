315a40e4942a0a9db0fbd7a38f22e545721e9ae1
Tejas Dinkar <tejas@gja.in>
2017-12-16 03:23:36 +0530

    Adding support for images and other assets

--- app/isomorphic/components/pages/home.js
+++ app/isomorphic/components/pages/home.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 import { StoryGrid } from "../story-grid.js";
+import img from '../../../assets/images/pw_maze_white.png'
+
+import assetify from '@quintype/framework/assetify';
 
 class HomePage extends React.Component {
   render() {
     return <div>
       <h1>Home</h1>
       <StoryGrid stories={this.props.data.stories} />
+      Here is an image:
+      <img src={assetify(img)} />
     </div>;
   }
 }
