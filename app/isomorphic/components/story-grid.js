4a9b9ea5cb5b8ba03a4c417b90b420480414bf4c
Tejas Dinkar <tejas@gja.in>
2018-02-15 18:38:34 +0530

    Adding our first CSS module

--- app/isomorphic/components/story-grid.js
+++ app/isomorphic/components/story-grid.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+import './story-grid.module.css';
 import { Link, ResponsiveImage, ClientSideOnly } from "@quintype/components";
 
 function StoryGridStoryItem(props) {
   return <Link href={"/" + props.story.slug} className="story-grid-item">
-      <figure className="story-grid-item-image qt-image-16x9">
+      <figure className="qt-image-16x9" styleName="story-grid-item-image">
         <ResponsiveImage slug={props.story["hero-image-s3-key"]} metadata={props.story["hero-image-metadata"]}
           aspectRatio={[16,9]}
           defaultWidth={480} widths={[250,480,640]} sizes="(max-width: 500px) 98%, (max-width: 768px) 48%, 23%"
