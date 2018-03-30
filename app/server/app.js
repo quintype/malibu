17c51f40a253b3dc24a16e360e5ad0e50f2c56f8
Prasanth Karri <prasanth.25k@gmail.com>
2018-03-30 10:39:01 +0530

    seo implementation for malibu

--- app/server/app.js
+++ app/server/app.js
@@ -10,10 +10,35 @@ import { generateRoutes, STATIC_ROUTES } from "./routes";
 import { renderLayout } from "./handlers/render-layout";
 import { loadData, loadErrorData } from "./load-data";
 import { pickComponent } from "../isomorphic/pick-component";
+import {SEO} from "@quintype/seo";
 
 export const app = createApp();
 
 upstreamQuintypeRoutes(app, { forwardAmp: true });
+
+const STATIC_TAGS = {
+  "twitter:site": "Quintype",
+  "twitter:domain": "quintype.com",
+  "twitter:app:name:ipad": undefined,
+  "twitter:app:name:googleplay": undefined,
+  "twitter:app:id:googleplay": undefined,
+  "twitter:app:name:iphone": undefined,
+  "twitter:app:id:iphone": undefined,
+  "apple-itunes-app": undefined,
+  "google-play-app": undefined,
+  "fb:app_id": undefined,
+  "fb:pages": undefined,
+  "og:site_name": "Quintype"
+};
+
+const STRUCTURED_DATA = {
+  organization: {
+    name: "Quintype",
+    url: "http://www.quintype.com/",
+    logo: "https://quintype.com/logo.png",
+    sameAs: ["https://www.facebook.com/quintype","https://twitter.com/quintype_in","https://plus.google.com/+quintype","https://www.youtube.com/user/Quintype"],
+  }
+}
 
 isomorphicRoutes(app, {
   appVersion: require("../isomorphic/app-version"),
@@ -24,7 +49,13 @@ isomorphicRoutes(app, {
   renderLayout: renderLayout,
   loadErrorData: loadErrorData,
   staticRoutes: STATIC_ROUTES,
-  seo: "",
+  seo: new SEO({
+    staticTags: STATIC_TAGS,
+    enableTwitterCards: true,
+    enableOgTags: true,
+    enableNews: true,
+    structuredData: STRUCTURED_DATA
+  }),
   preloadJs: true,
   preloadRouteData: true
 });
