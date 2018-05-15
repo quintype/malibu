b02c236bb3fe7494f38350c862c3ed15534d038e
Tejas Dinkar <tejas@gja.in>
2018-05-16 01:30:12 +0530

    Adding a sample test

---
+++ app/isomorphic/components/__test__/sample.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+function Component({page, children}) {
+  return <a href={page}>{children}</a>;
+}
+
+test('adds 1 + 2 to equal 3', () => {
+  const component = renderer.create(
+    <Component page="http://www.facebook.com">Facebook</Component>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
