a370f45466179af37858581b31fe4b188a07a249
Deokumarjnu <deo.kumar.jnu@gmail.com>
2018-09-04 15:58:47 +0530

    removed unused files and add package.json and package.lock

--- app/isomorphic/components/layouts/icon.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./icon.m.css";
-
-let Icon = props => {
-  return <a href="#" key={props.icontype} className={`fa fa-${props.icontype}`} styleName={props.icontype}></a>;
-}
-
-export {Icon}
