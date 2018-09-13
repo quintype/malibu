6866e82b35cd3c567254f3c858c30c2131e8cf87
Garima Kamboj <garima.kamboj@icloud.com>
2018-09-13 13:07:50 +0530

    Update framework in package-lock
    Add analyse-stats

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
 const { webpackConfig } = require("@quintype/build/webpack-config");
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = Object.assign({node: {Buffer: false}}, webpackConfig("quintype-malibu", __dirname));
+const config = Object.assign({node: {Buffer: false}}, webpackConfig("quintype-malibu", __dirname));
+
+if (process.env.ANALYZE_STATS === 'true') {
+  config.plugins.push(new BundleAnalyzerPlugin({
+    generateStatsFile: true,
+    analyzerMode: "static",
+    statsFilename: "prod-stats.json"
+  }));
+}
+
+module.exports = config;
