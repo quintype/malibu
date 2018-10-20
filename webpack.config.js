6c41761504b061cdbde244c4a08ee3c38a386c99
Amith George <strider.am@gmail.com>
2018-10-20 23:08:47 +0530

    add webpack plugin webpack-common-shake
    https://github.com/indutny/webpack-common-shake
    
    this reduced the bundle size by 1.1kb

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const { webpackConfig } = require("@quintype/build/webpack-config");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const ShakePlugin = require("webpack-common-shake").Plugin;
 
-const config = Object.assign({node: {Buffer: false}}, webpackConfig("quintype-malibu", __dirname));
+const config = Object.assign(
+  { node: { Buffer: false } },
+  webpackConfig("quintype-malibu", __dirname)
+);
 
-if (process.env.ANALYZE_STATS === 'true') {
-  config.plugins.push(new BundleAnalyzerPlugin({
-    generateStatsFile: true,
-    analyzerMode: "static",
-    statsFilename: "prod-stats.json"
-  }));
+config.plugins.push(new ShakePlugin());
+
+if (process.env.ANALYZE_STATS === "true") {
+  config.plugins.push(
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
+      analyzerMode: "static",
+      statsFilename: "prod-stats.json"
+    })
+  );
 }
 
 module.exports = config;
