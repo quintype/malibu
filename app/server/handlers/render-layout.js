a7b2163160daf9cdcee55f34c88f424fa58280c2
Tejas Dinkar <tejas@gja.in>
2018-10-22 16:19:22 +0530

    Malibu supports multiple chunks for page types

--- app/server/handlers/render-layout.js
+++ app/server/handlers/render-layout.js
@@ -1,9 +1,26 @@
 /* eslint-disable object-shorthand */
-import { assetPath, readAsset } from "@quintype/framework/server/asset-helper";
+import { assetPath, readAsset, assetFiles } from "@quintype/framework/server/asset-helper";
+import { COMPONENTS, getChunkName } from '../../isomorphic/pick-component'
 
 const cssContent = assetPath("app.css") ? readAsset("app.css") : "";
 
+// TODO: Move this inside assetHelper
+function loadChunk(acc, {chunk}) {
+  acc[chunk] = acc[chunk] || {
+    cssPath: assetPath(`${chunk}.css`),
+    cssContent: readAsset(`${chunk}.css`),
+    jsPath: [assetPath(`${chunk}.js`), assetPath(`vendors~${chunk}.js`)],
+  };
+  return acc;
+}
+const allChunks = Object.values(COMPONENTS).reduce(loadChunk, {});
+
 export function renderLayout(res, params) {
+  const chunk = allChunks[getChunkName(params.pageType)];
+  if(chunk) {
+    res.append("Link", chunk.jsPath.map(x => `<${x}>; rel=preload; as=script`).join(", "))
+  }
+
   res.render(
     "pages/layout",
     Object.assign(
@@ -14,7 +31,8 @@ export function renderLayout(res, params) {
         contentTemplate: null,
         title: params.title,
         disableAjaxNavigation: false,
-        metaTags: params.seoTags ? params.seoTags.toString() : ""
+        metaTags: params.seoTags ? params.seoTags.toString() : "",
+        pageChunk: chunk,
       },
       params
     )
