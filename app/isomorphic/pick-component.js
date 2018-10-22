082fd999c222882691acfb9ec6bc1d4d3e3df40b
Tejas Dinkar <tejas@gja.in>
2018-10-22 15:53:06 +0530

    First cut of async rendering

--- app/isomorphic/pick-component.js
+++ app/isomorphic/pick-component.js
@@ -1,38 +1,31 @@
 import { PAGE_TYPE } from "./constants";
 
-import { HomePage } from "./components/pages/home";
-import { SectionPage } from "./components/pages/section";
-import { TagPage } from "./components/pages/tag";
-import { SearchPage } from "./components/pages/search";
-import { StoryPage } from "./components/pages/story";
-import { CatalogPage } from "./components/pages/catalog";
-import { StoryPagePreview } from "./components/pages/story-preview";
-import { HomePagePreview } from "./components/pages/home-preview";
-import { NotFoundPage } from "./components/pages/not-found";
+export const COMPONENTS = {
+  [PAGE_TYPE.HOME_PAGE]: {chunk: "list", component: "HomePage"},
+  [PAGE_TYPE.SECTION_PAGE]: {chunk: "list", component: "SectionPage"},
+  [PAGE_TYPE.TAG_PAGE]: {chunk: "list", component: "TagPage"},
+  [PAGE_TYPE.SEARCH_PAGE]: {chunk: "list", component: "SearchPage"},
+  [PAGE_TYPE.STORY_PAGE]: {chunk: "story", component: "StoryPage"},
+  [PAGE_TYPE.CATALOG_PAGE]: {chunk: "list", component: "CatalogPage"},
+  [PAGE_TYPE.STORY_PREVIEW]: {chunk: "story", component: "StoryPagePreview"},
+  [PAGE_TYPE.STORY_PUBLIC_PREVIEW_PAGE]: {chunk: "story", component: "StoryPage"},
+  [PAGE_TYPE.HOME_PREVIEW]: {chunk: "list", component: "HomePagePreview"},
+  "default": {chunk: "list", component: "NotFoundPage"},
+}
 
-function pickComponent(pageType) {
-  switch (pageType) {
-    case PAGE_TYPE.HOME_PAGE:
-      return HomePage;
-    case PAGE_TYPE.SECTION_PAGE:
-      return SectionPage;
-    case PAGE_TYPE.TAG_PAGE:
-      return TagPage;
-    case PAGE_TYPE.SEARCH_PAGE:
-      return SearchPage;
-    case PAGE_TYPE.STORY_PAGE:
-      return StoryPage;
-    case PAGE_TYPE.CATALOG_PAGE:
-      return CatalogPage;
-    case PAGE_TYPE.STORY_PREVIEW:
-      return StoryPagePreview;
-    case PAGE_TYPE.STORY_PUBLIC_PREVIEW_PAGE:
-      return StoryPage;
-    case PAGE_TYPE.HOME_PREVIEW:
-      return HomePagePreview;
-    default:
-      return NotFoundPage;
+function importChunk(chunkName) {
+  switch(chunkName) {
+    case 'list': return import(/* webpackChunkName: "list" */ './component-bundles/list.js');
+    case 'story': return import(/* webpackChunkName: "story" */ './component-bundles/story.js');
   }
 }
 
-export { pickComponent };
+export function getChunkName(pageType) {
+  const {chunk, component} = COMPONENTS[pageType] || COMPONENTS["default"];
+  return chunk;
+}
+
+export function pickComponent(pageType) {
+  const {chunk, component} = COMPONENTS[pageType] || COMPONENTS["default"];
+  return importChunk(chunk).then(chunk => chunk[component]);
+}
