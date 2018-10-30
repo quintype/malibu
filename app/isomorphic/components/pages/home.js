c1e781ead6824a656e196de888686f99b05d147c
Tejas Dinkar <tejas@gja.in>
2018-10-30 06:39:45 +0530

    Removing ad from malibu homepage

--- app/isomorphic/components/pages/home.js
+++ app/isomorphic/components/pages/home.js
@@ -4,25 +4,13 @@ import assetify from "@quintype/framework/assetify";
 
 import { getCollectionTemplate } from "../get-collection-template";
 import img from "../../../assets/images/pw_maze_white.png";
-import { DfpAd } from "../dfp-ad";
 import { Collection } from "@quintype/components";
 
-const HomePage = props => (
+export const HomePage = props => (
   <div>
     <h1>Home</h1>
     <Collection
       collection={props.data.collection}
-      collectionTemplates={getCollectionTemplate}
-    />
-    <p>
-      Here is an image: <img src={assetify(img)} border={1} alt="some image" />
-    </p>
-    <br />
-    <p>
-      Here is an ad: <DfpAd adtype="homepage-banner" />
-    </p>
-    <br />
+      collectionTemplates={getCollectionTemplate} />
   </div>
 );
-
-export { HomePage };
