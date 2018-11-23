d7878db0bb6315b3b453c424742d0b79a487bef2
Tejas Dinkar <tejas@gja.in>
2018-11-23 15:06:30 +0530

    Whitespace

--- app/isomorphic/pick-component.js
+++ app/isomorphic/pick-component.js
@@ -9,22 +9,14 @@ const { pickComponent, getChunkName } = pickComponentHelper(
     [PAGE_TYPE.SEARCH_PAGE]: { chunk: "list", component: "SearchPage" },
     [PAGE_TYPE.STORY_PAGE]: { chunk: "story", component: "StoryPage" },
     [PAGE_TYPE.CATALOG_PAGE]: { chunk: "list", component: "CatalogPage" },
-    [PAGE_TYPE.STORY_PREVIEW]: {
-      chunk: "story",
-      component: "StoryPagePreview"
-    },
-    [PAGE_TYPE.STORY_PUBLIC_PREVIEW_PAGE]: {
-      chunk: "story",
-      component: "StoryPage"
-    },
+    [PAGE_TYPE.STORY_PREVIEW]: { chunk: "story", component: "StoryPagePreview" },
+    [PAGE_TYPE.STORY_PUBLIC_PREVIEW_PAGE]: { chunk: "story", component: "StoryPage" },
     [PAGE_TYPE.HOME_PREVIEW]: { chunk: "list", component: "HomePagePreview" },
     default: { chunk: "list", component: "NotFoundPage" }
   },
   {
-    list: () =>
-      import(/* webpackChunkName: "list" */ "./component-bundles/list.js"),
-    story: () =>
-      import(/* webpackChunkName: "story" */ "./component-bundles/story.js")
+    list: () => import(/* webpackChunkName: "list" */ "./component-bundles/list.js"),
+    story: () => import(/* webpackChunkName: "story" */ "./component-bundles/story.js")
   }
 );
 
