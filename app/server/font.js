1907b06237b2d364c85f13d741e6781255b59459
Tejas Dinkar <tejas@gja.in>
2019-10-29 17:20:09 +0530

    Update font.js

--- app/server/font.js
+++ app/server/font.js
@@ -1,6 +1,6 @@
-const DEFAULT_FONT_CONFIG = Object.freeze({
-  "title-font": "Lato, sans-serif",
-  "content-font": "Roboto, sans-serif"
+const FONTS = Object.freeze({
+  "title-font": { value: "Lato, sans-serif", fallback: "sans-serif" },
+  "content-font": { value: "Roboto, sans-serif", fallback: "sans-serif" }
 });
 
 export default {
@@ -10,8 +10,5 @@ export default {
     { fontName: "Roboto", data: { weight: 400 } },
     { fontName: "Roboto", data: { weight: 700 } }
   ],
-  fontSettings: Object.assign({}, DEFAULT_FONT_CONFIG, {
-    "title-font": { value: "Lato, sans-serif", fallback: "sans-serif" },
-    "content-font": { value: "Roboto, sans-serif", fallback: "sans-serif" }
-  })
+  fontSettings: FONTS
 };
