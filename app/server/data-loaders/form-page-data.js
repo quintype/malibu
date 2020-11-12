97ba6403456fcdfe88699db8dc3b6d01b7075a2e
Sinu john <sinu@quintype.com>
2020-11-12 13:50:22 +0530

    add cacheKeys

--- app/server/data-loaders/form-page-data.js
+++ app/server/data-loaders/form-page-data.js
@@ -1,7 +1,7 @@
 export function loadFormPageData(client, formSlug, next) {
   return client
     .request(`/api/v1/forms/${formSlug}`)
-    .then(result => ({ form: result.data }))
+    .then(result => ({ form: result.data, cacheKeys: result.cacheKeys }))
     .catch(response => {
       return response.statusCode === 404 ? next() : Promise.reject(response);
     });
