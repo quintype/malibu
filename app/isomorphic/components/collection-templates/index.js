353ccfb946e565a82751823c82e11a2b44d459cf
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2020-11-30 14:20:07 +0530

    rtl support for home page rows

--- app/isomorphic/components/collection-templates/index.js
+++ app/isomorphic/components/collection-templates/index.js
@@ -1,27 +1,114 @@
 import { wrapCollectionLayout, EagerLoadImages } from "@quintype/components";
-import { FourColGrid } from "./four-col-grid";
+// import { FourColGrid } from "./four-col-grid";
 import React from "react";
+import { shallowEqual, useSelector } from "react-redux";
+import get from "lodash/get";
+import "./arrow.m.css";
+import {
+  FourColGrid,
+  OneColStoryList,
+  ThreeColGrid,
+  TwoColFourStories,
+  TwoColThreeStories,
+  ThreeColSixStories,
+  ThreeColSevenStory,
+  ElevenStories,
+  CollectionFilter,
+  HalfScreenSlider,
+  FourStorySlider,
+  FullScreenSlider,
+  ThreeColFourteenStories,
+  FourColTwelveStories
+} from "@quintype/arrow";
 
-// This should not be needed anymore as we are using Gumlet
-function wrapEager(f) {
-  const wrappedComponent = function WrapEager(props) {
-    if (props.index === 0) {
-      return (
-        <EagerLoadImages predicate={token => token === "above-fold"}>{React.createElement(f, props)}</EagerLoadImages>
-      );
-    } else {
-      return React.createElement(f, props);
-    }
+const arrowWrapper = Component => ({ collection, metadata = {} }) => {
+  const pageType = useSelector(state => get(state, ["qt", "pageType"], ""), shallowEqual);
+  const supportedPages = {
+    "home-page": "home",
+    "section-page": "section",
+    "collection-page": "section",
+    "tag-page": "tag",
+    "author-page": "author",
+    "pagebuilder-section-preview-page": "section",
+    "pagebuilder-collection-preview-page": "section",
+    "pagebuilder-story-preview-page": "story",
+    "authors-page": "authors",
+    "": ""
   };
 
-  if (f.storyLimit) {
-    wrappedComponent.storyLimit = f.storyLimit;
-  }
+  const theme = useSelector(
+    state => get(state, ["qt", "config", "pagebuilder-config", supportedPages[pageType], "pageSetting", "bgColor"], ""),
+    shallowEqual
+  );
 
-  return wrappedComponent;
-}
+  const { "pagebuilder-config": pbconfig, "theme-attributes": themeAttributes } = useSelector(state =>
+    get(state, ["qt", "config"])
+  );
+  const fallbackImageUrl = get(
+    pbconfig,
+    ["general", "fallbackImage", "url"],
+    get(themeAttributes, ["imageFallbackUrl"])
+  );
+
+  const config = get(metadata, ["config"], {});
+
+  const updateWithComponent = config => {
+    const pbRowsConfig = useSelector(state =>
+      get(state, ["qt", "config", "pagebuilder-config", "general", "rows"], {})
+    );
+
+    // Arrow needs the config values like sectionTagTemplate directly under config prop.
+    // TODO: May need to flatten the rowConfig sometime.
+    const sectionTagTemplate = get(
+      pbRowsConfig,
+      ["sectionTag", "template"],
+      get(pbRowsConfig, ["sectionTagTemplate"], "solid")
+    );
+
+    const collectionNameTemplate = get(pbRowsConfig, ["collectionNameTemplate"], "");
+    const borderColor = get(pbRowsConfig, ["sectionTag", "color"], "");
+    const collectionNameBorderColor = get(pbRowsConfig, ["titleStyle", "underLineColor"], "");
+    // borderColor to change sectionTag color. TODO: rename to sectionTagColor
+
+    const supportedPagesForTitle = ["author-page", "tag-page"];
+
+    const pages = supportedPagesForTitle.includes(pageType);
+
+    let updatedConfig = {
+      theme,
+      ...config,
+      sectionTagTemplate,
+      collectionNameTemplate,
+      borderColor,
+      collectionNameBorderColor,
+      fallbackImageUrl,
+      ...(pages && { showRowTitle: false })
+    };
+
+    return updatedConfig;
+  };
+
+  return (
+    <div style={{ maxWidth: "1172px", margin: "auto" }}>
+      <Component collection={collection} config={updateWithComponent(config)} />
+    </div>
+  );
+};
 
 export default {
-  FourColGrid: wrapEager(wrapCollectionLayout(FourColGrid)),
-  defaultTemplate: wrapEager(wrapCollectionLayout(FourColGrid))
+  ArrowFourColGrid: arrowWrapper(FourColGrid),
+  ArrowCollectionFilter: arrowWrapper(CollectionFilter),
+  ArrowThreeColGrid: arrowWrapper(ThreeColGrid),
+  ArrowOneColStoryList: arrowWrapper(OneColStoryList),
+  ArrowThreeColSixStories: arrowWrapper(ThreeColSixStories),
+  ArrowTwoColThreeStories: arrowWrapper(TwoColThreeStories),
+  ArrowElevenStories: arrowWrapper(ElevenStories),
+  ArrowTwoColFourStories: arrowWrapper(TwoColFourStories),
+  ArrowHalfScreenSlider: arrowWrapper(HalfScreenSlider),
+  ArrowFullScreenSlider: arrowWrapper(FullScreenSlider),
+  ArrowFourStorySlider: arrowWrapper(FourStorySlider),
+  ArrowThreeColSevenStory: arrowWrapper(ThreeColSevenStory),
+  ArrowFourColTwelveStories: arrowWrapper(FourColTwelveStories),
+  ArrowThreeColFourteenStories: arrowWrapper(ThreeColFourteenStories),
+  defaultTemplate: arrowWrapper(CollectionFilter)
 };
