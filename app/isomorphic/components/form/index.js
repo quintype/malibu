7c16c846464dd9cfb9e8017e4913c43ebbd12e6c
Sinu John <sinujohn91@gmail.com>
2020-12-01 13:46:46 +0530

    feat(forms): Add support for rendering form pages (#518)
    * add form route
    
    * add nvmrc file
    
    * handle 404 when form is not available
    
    * isolate bootstrap for forms
    
    * add cacheKeys
    
    * add support for closed forms
    
    Co-authored-by: Sinu john <sinu@quintype.com>

---
+++ app/isomorphic/components/form/index.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { string, bool } from "prop-types";
+
+import styles from "./styles.m.css";
+
+const Form = ({ formioUrl, disabled }) => {
+  const [Formio, setFormComponent] = useState({});
+  useEffect(() => {
+    // SSR is not supported by react-formio
+    import(/* webpackChunkName: "qtc-react-formio" */ "react-formio")
+      .then(Formio => {
+        setFormComponent({ component: Formio.Form });
+      })
+      .catch(error => console.error("Error loading formio component", error));
+  }, []);
+
+  return (
+    <div className={`bootstrap ${styles["form-container"]}`}>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://toert.github.io/Isolated-Bootstrap/versions/4.0.0-beta/iso_bootstrap4.0.0min.css"
+      />
+      <link rel="stylesheet" href="https://unpkg.com/formiojs@latest/dist/formio.full.min.css" />
+      {Formio.component && <Formio.component src={formioUrl} options={{ readOnly: disabled }} />}
+    </div>
+  );
+};
+
+Form.propTypes = {
+  formioUrl: string,
+  disabled: bool
+};
+
+export { Form };
---
+++ app/isomorphic/components/pages/form.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { shape, string } from "prop-types";
+
+import { Form } from "../form";
+
+const FormPage = props => {
+  const { headline, subheadline, status, "formio-url": formioUrl } = props.data.form;
+  const isClosed = status === "closed";
+  return (
+    <div>
+      <h1>{headline}</h1>
+      <h2>{subheadline}</h2>
+      {isClosed && <p>The form is closed for submissions.</p>}
+      <Form formioUrl={formioUrl} disabled={isClosed} />
+    </div>
+  );
+};
+
+FormPage.propTypes = {
+  data: shape({
+    form: {
+      "formio-url": string,
+      headline: string,
+      subheadline: string,
+      status: string
+    }
+  })
+};
+
+export { FormPage };
