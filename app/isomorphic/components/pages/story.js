ed916089bfa99be83db188e8c8f820d410b52dab
Deokumarjnu <deo.kumar.jnu@gmail.com>
2020-12-14 12:49:50 +0530

    Added story template from arrow

--- app/isomorphic/components/pages/story.js
+++ app/isomorphic/components/pages/story.js
@@ -1,19 +1,128 @@
-/* eslint-disable no-unused-vars, no-console, react/jsx-indent-props,react/jsx-wrap-multilines, no-undef, react/jsx-closing-bracket-location */
-
 import React from "react";
 import { InfiniteStoryBase, WithPreview } from "@quintype/components";
-import { BlankStory } from "../story-templates/blank";
-import { number, object, shape, any } from "prop-types";
-
-function StoryPageBase({ index, story, otherProp }) {
+// import { BlankStory } from "../story-templates/blank";
+import { number, object, shape, any, array } from "prop-types";
+import {
+  LiveBlogStoryTemplate,
+  ListicleStoryTemplate,
+  VideoStoryTemplate,
+  TextStoryTemplate,
+  PhotoStoryTemplates
+} from "@quintype/arrow";
+// import pho
+function StoryPageBase({ index, story, otherProp, trending }) {
+  console.log("here trending", trending);
   // Can switch to a different template based story-template, or only show a spoiler if index > 0
-  return <BlankStory story={story} />;
+  const templateConfigcommon = {
+    theme: "#ffffff",
+    sort: "image-first",
+    noOfVisibleCards: -1,
+    publishedDetails: {
+      enablePublishedTime: true,
+      enableUpdatedTime: true,
+      enableReadTime: true
+    },
+    asideCollection: {
+      data: trending,
+      config: {
+        collectionNameBorderColor: "#3a9fdd",
+        title: "Trending",
+        theme: "#ffffff"
+      }
+    },
+    authorDetails: {
+      template: "default",
+      opts: {
+        showBio: true,
+        showImage: true
+      }
+    }
+  };
+  const storyElementsConfig = {
+    summary: {},
+    blurb: {},
+    blockquote: {},
+    quote: {},
+    "also-read": {},
+    "q-and-a": {},
+    question: {},
+    answer: {},
+    references: {}
+  };
+  const templateConfigLiveBlog = {
+    theme: "#ffffff",
+    templateType: "default",
+    noOfVisibleCards: -1,
+    showSectionTag: true,
+    buttonText: "Load More",
+    publishedDetails: {
+      enablePublishedTime: true,
+      enableUpdatedTime: true
+    },
+    asideCollection: {
+      data: trending,
+      config: {
+        title: "Trending Stories",
+        theme: "#ffffff",
+        showAuthor: true,
+        showTime: true
+      }
+    },
+    authorDetails: {
+      template: "default",
+      opts: {
+        hideBio: false,
+        hideImage: false
+      }
+    }
+  };
+  switch (story["story-template"]) {
+    case "live-blog":
+      return (
+        <div className="full-width-with-padding separator">
+          <LiveBlogStoryTemplate
+            story={story}
+            config={templateConfigLiveBlog}
+            storyElementsConfig={storyElementsConfig}
+          />
+        </div>
+      );
+    case "listicle":
+      return (
+        <div className="full-width-with-padding separator">
+          <ListicleStoryTemplate
+            story={story}
+            config={templateConfigcommon}
+            storyElementsConfig={storyElementsConfig}
+          />
+        </div>
+      );
+    case "video":
+      return (
+        <div className="full-width-with-padding separator">
+          <VideoStoryTemplate story={story} config={templateConfigcommon} storyElementsConfig={storyElementsConfig} />
+        </div>
+      );
+    case "photo":
+      return (
+        <div className="full-width-with-padding separator">
+          <PhotoStoryTemplates story={story} config={templateConfigcommon} storyElementsConfig={storyElementsConfig} />
+        </div>
+      );
+    default:
+      return (
+        <div className="full-width-with-padding separator">
+          <TextStoryTemplate story={story} config={templateConfigcommon} storyElementsConfig={storyElementsConfig} />
+        </div>
+      );
+  }
 }
 
 StoryPageBase.propTypes = {
   index: number,
   story: object,
-  otherProp: any
+  otherProp: any,
+  trending: array
 };
 
 const FIELDS =
@@ -37,7 +146,7 @@ export function StoryPage(props) {
       render={StoryPageBase}
       loadItems={storyPageLoadItems}
       onInitialItemFocus={item =>
-        app.registerPageView({ pageType: "story-page", data: { story: item.story } }, `/${item.story.slug}`)
+        window.app.registerPageView({ pageType: "story-page", data: { story: item.story } }, `/${item.story.slug}`)
       }
       onItemFocus={item => console.log(`Story In View: ${item.story.headline}`)}
     />
