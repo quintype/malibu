d9581be5d922e1ab4790fb8c2d3e45c7e6753987
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2020-12-15 06:41:03 +0530

    implement listing pages with arrow library

--- app/isomorphic/components/story-grid.js
+++ app/isomorphic/components/story-grid.js
@@ -1,28 +1,29 @@
 import React from "react";
-import { Link, ResponsiveImage } from "@quintype/components";
+// import { Link, ResponsiveImage } from "@quintype/components";
 import PT from "prop-types";
+import { StoryCard } from "@quintype/arrow";
 import "./story-grid.m.css";
 
-function StoryGridStoryItem(props) {
-  return (
-    <Link href={`${props.story.url}`} className="story-grid-item">
-      <figure className="qt-image-16x9" styleName="story-grid-item-image">
-        <ResponsiveImage
-          slug={props.story["hero-image-s3-key"]}
-          metadata={props.story["hero-image-metadata"]}
-          aspectRatio={[16, 9]}
-          defaultWidth={480}
-          widths={[250, 480, 640]}
-          sizes="( max-width: 500px ) 98vw, ( max-width: 768px ) 48vw, 23vw"
-          imgParams={{ auto: ["format", "compress"] }}
-          eager={props.position < 2 ? "above-fold" : "below-fold"}
-        />
-      </figure>
-      <h2>{props.story.headline}</h2>
-      <span className="story-grid-item-author">{props.story["author-name"]}</span>
-    </Link>
-  );
-}
+// function StoryGridStoryItem(props) {
+//   return (
+//     <Link href={`${props.story.url}`} className="story-grid-item">
+//       <figure className="qt-image-16x9" styleName="story-grid-item-image">
+//         <ResponsiveImage
+//           slug={props.story["hero-image-s3-key"]}
+//           metadata={props.story["hero-image-metadata"]}
+//           aspectRatio={[16, 9]}
+//           defaultWidth={480}
+//           widths={[250, 480, 640]}
+//           sizes="( max-width: 500px ) 98vw, ( max-width: 768px ) 48vw, 23vw"
+//           imgParams={{ auto: ["format", "compress"] }}
+//           eager={props.position < 2 ? "above-fold" : "below-fold"}
+//         />
+//       </figure>
+//       <h2>{props.story.headline}</h2>
+//       <span className="story-grid-item-author">{props.story["author-name"]}</span>
+//     </Link>
+//   );
+// }
 
 const storyPropType = PT.shape({
   id: PT.string,
@@ -33,16 +34,17 @@ const storyPropType = PT.shape({
   "author-name": PT.string
 });
 
-StoryGridStoryItem.propTypes = {
-  story: storyPropType,
-  position: PT.integer
-};
+// StoryGridStoryItem.propTypes = {
+//   story: storyPropType,
+//   position: PT.integer
+// };
 
 export function StoryGrid(props) {
   return (
     <div className="story-grid">
       {props.stories.map((story, index) => (
-        <StoryGridStoryItem story={story} key={`${index}-${story.id}`} position={index} />
+        <StoryCard story={story} key={`${index}-${story.id}`} />
+        // <StoryGridStoryItem story={story} key={`${index}-${story.id}`} position={index} />
       ))}
     </div>
   );
