2addf53dce4fec1f213dbc758e79b972eb8df6d4
nandap4790 <nandap4790@gmail.com>
2020-12-17 14:51:49 +0530

    test

--- app/server/data-loaders/tag-page-data.js
+++ app/server/data-loaders/tag-page-data.js
@@ -3,27 +3,23 @@ import { Story } from "@quintype/framework/server/api-client";
 import { storyToCacheKey } from "@quintype/framework/server/caching";
 import { TAG_PAGE_URL_PREFIX } from "../../isomorphic/constants";
 
-export function loadTagPageData(client, tagSlug, config) {
-  return Story.getStories(client, "top", { "tag-slugs": tagSlug, limit: "20" }).then(
-    stories => {
-      return client.getTags(tagSlug).then(({ tags }) => {
-        const tagName = get(tags, [0, "name"], tagSlug);
-        const tagDescription = get(tags, [0, "meta-description"], tagSlug);
-        const tag = {
-          slug: tagSlug,
-          name: tagName,
-          description: tagDescription,
-          path: `${TAG_PAGE_URL_PREFIX}${tagSlug}`
-        };
-        return {
-          stories: stories.map(story => story.asJson()),
-          title: tagName,
-          cacheKeys: stories.map(story =>
-            storyToCacheKey(config["publisher-id"], story)
-          ),
-          tag
-        };
-      });
-    }
-  );
+export function loadTagPageData(client, config) {
+  return Story.getStories(client, "top", { "tag-slugs": tagSlug, limit: "20" }).then(stories => {
+    return client.getTags(tagSlug).then(({ tags }) => {
+      const tagName = get(tags, [0, "name"], tagSlug);
+      const tagDescription = get(tags, [0, "meta-description"], tagSlug);
+      const tag = {
+        slug: tagSlug,
+        name: tagName,
+        description: tagDescription,
+        path: `${TAG_PAGE_URL_PREFIX}${tagSlug}`
+      };
+      return {
+        stories: stories.map(story => story.asJson()),
+        title: tagName,
+        cacheKeys: stories.map(story => storyToCacheKey(config["publisher-id"], story)),
+        tag
+      };
+    });
+  });
 }
