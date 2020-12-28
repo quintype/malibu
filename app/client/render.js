e79809e1c0b9fd24510647809c15af7f52a6ff2b
Athira MR <athiramraju001@gmail.com>
2020-12-28 19:19:32 +0530

    fix(remove hydration for header)

--- app/client/render.js
+++ app/client/render.js
@@ -1,12 +1,12 @@
 import { renderIsomorphicComponent, renderComponent, renderBreakingNews } from "@quintype/framework/client/start";
 import { pickComponent } from "../isomorphic/pick-component";
 import { BreakingNewsView } from "../isomorphic/components/breaking-news-view";
-import { Header } from "../isomorphic/components/header";
+// import { Header } from "../isomorphic/components/header";
 import { Footer } from "../isomorphic/components/layouts/footer";
 
 export function preRenderApplication(store) {
   const hydrate = { hydrate: !global.qtLoadedFromShell };
-  renderComponent(Header, "header", store, hydrate);
+  // renderComponent(Header, "header", store, hydrate);
   renderBreakingNews("breaking-news-container", store, BreakingNewsView, hydrate);
   renderComponent(Footer, "footer", store, hydrate);
 }
