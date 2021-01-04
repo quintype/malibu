68c2418af88d8cbda496c20d6612d72773e3f003
Ramsharan Gorur Jayaraman <sharangj@gmail.com>
2021-01-04 12:05:12 +0530

    Integrate loadable

--- app/server/handlers/render-layout.js
+++ app/server/handlers/render-layout.js
@@ -1,18 +1,33 @@
 /* eslint-disable object-shorthand */
-import { assetPath, readAsset, getAllChunks } from "@quintype/framework/server/asset-helper";
-import { getChunkName } from "../../isomorphic/pick-component";
+import { ChunkExtractor } from "@loadable/server";
+import { assetPath, getAllChunks, readAsset } from "@quintype/framework/server/asset-helper";
 import { renderReduxComponent } from "@quintype/framework/server/render";
+import path from "path";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import { Provider } from "react-redux";
+import serialize from "serialize-javascript";
+import { BreakingNewsView } from "../../isomorphic/components/breaking-news-view";
 import { Header } from "../../isomorphic/components/header";
 import { Footer } from "../../isomorphic/components/layouts/footer";
+import { getChunkName } from "../../isomorphic/pick-component";
 import fontFace from "../font";
-import { BreakingNewsView } from "../../isomorphic/components/breaking-news-view";
-import serialize from "serialize-javascript";
 
+const statsFile = path.resolve("stats.json");
 const cssContent = assetPath("app.css") ? readAsset("app.css") : "";
 const fontJsContent = assetPath("font.js") ? readAsset("font.js") : "";
 const allChunks = getAllChunks("list", "story");
 
+function renderLoadableReduxComponent(Component, store, extractor, props) {
+  const comp = extractor.collectChunks(React.createElement(Provider, { store }, React.createElement(Component, props)));
+  const string = ReactDOMServer.renderToString(comp);
+  console.log(extractor.getStyleElements());
+  return string;
+}
+
 export function renderLayout(res, params) {
+  const extractor = new ChunkExtractor({ statsFile });
+  const header = renderLoadableReduxComponent(Header, params.store, extractor);
   const chunk = params.shell ? null : allChunks[getChunkName(params.pageType)];
 
   res.render(
--- quintype-build.config.js
+++ quintype-build.config.js
@@ -1,1 +1,31 @@
-module.exports = require("@quintype/build/config/quintype-build");
+const { produce } = require("immer");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const LoadablePlugin = require("@loadable/webpack-plugin");
+const path = require("path");
+module.exports = {
+  modifyWebpackConfig: function({ defaultConfig }) {
+    const config = produce(defaultConfig, function(draft) {
+      draft.node = { Buffer: false };
+      draft.entry["font"] = "./app/client/font.js";
+      if (process.env.ANALYZE_STATS === "true") {
+        draft.plugins.push(
+          new BundleAnalyzerPlugin({
+            generateStatsFile: true,
+            analyzerMode: "static",
+            statsFilename: "prod-stats.json"
+          })
+        );
+      }
+    });
+    return {
+      ...config,
+      plugins: config.plugins.concat([new LoadablePlugin({ writeToDisk: true, filename: path.resolve("stats.json") })])
+    };
+  },
+  modifyBabelConfig: function({ defaultConfig, babelTarget, env }) {
+    const config = produce(defaultConfig, draft => {
+      draft.plugins.push(["@loadable/babel-plugin"]);
+    });
+    return { ...config };
+  }
+};
