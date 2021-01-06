87b31601488a7ad7b549398dd7ea53f3e6c69a9a
Athira MR <athiramraju001@gmail.com>
2021-01-06 14:59:35 +0530

    fix(footer implementation)

--- app/isomorphic/components/layouts/footer/index.js
+++ app/isomorphic/components/layouts/footer/index.js
@@ -1,49 +1,130 @@
 import React from "react";
-import PT from "prop-types";
 import { connect } from "react-redux";
+import { array, string } from "prop-types";
 import get from "lodash/get";
 import { Link } from "@quintype/components";
-import "./styles.m.css";
+import assetify from "@quintype/framework/assetify";
 
-const FooterBase = ({ footerLinks }) => (
-  <div styleName="footer">
-    {footerLinks.map(
-      item =>
-        item.isExternalLink ? (
-          <a
-            href={item.completeUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            styleName="link"
-          >
-            {item.title}
-          </a>
-        ) : (
-          <Link href={item.completeUrl} styleName="link">
-            {item.title}
-          </Link>
-        )
-    )}
-  </div>
-);
+import Logo from "../../basic/images/logo-white.svg";
+import SocialMediaIcons from "../../atoms/social-media-icon";
 
-function mapStateToProps(state) {
-  return {
-    footerLinks: get(state, ["qt", "data", "navigationMenu", "footerLinks"], [])
-  };
-}
+import "./footer.m.css";
 
-FooterBase.propTypes = {
-  footerLinks: PT.arrayOf(
-    PT.shape({
-      isExternalLink: PT.bool,
-      completeUrl: PT.string,
-      title: PT.string
-    })
-  )
+const StaticFooterLinks = ({ staticFooterLinks }) => {
+  return (
+    <ul styleName="static-link">
+      {staticFooterLinks.length >= 1 &&
+        staticFooterLinks.map(item => {
+          return item.isExternalLink ? (
+            <li styleName="static-link-list" key={item.title}>
+              <a href={item.completeUrl} styleName="static-link-item">
+                {item.title}
+              </a>
+            </li>
+          ) : (
+            <li styleName="static-link-list" key={item.title}>
+              <Link href={item.completeUrl || "/"} styleName="static-link-item">
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+    </ul>
+  );
 };
 
-export const Footer = connect(
-  mapStateToProps,
-  null
-)(FooterBase);
+const FooterMenuLinks = ({ footerMenuLinks }) => {
+  return (
+    <React.Fragment>
+      {footerMenuLinks.length >= 1 && (
+        <ul styleName="menu-link">
+          <li styleName="trending-tags">Trending Tags</li>
+          {footerMenuLinks.map(item => {
+            return item.isExternalLink ? (
+              <li key={item} styleName="footer-item">
+                <a styleName="link" rel="noopener noreferrer" href={item.completeUrl}>
+                  {item.title}
+                </a>
+              </li>
+            ) : (
+              <li key={item} styleName="footer-item">
+                <Link href={item.completeUrl || "/"} styleName="link">
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </React.Fragment>
+  );
+};
+
+const Copyright = ({ copyrightText = "" }) => {
+  return (
+    <ul styleName="copy-right">
+      <li styleName="copy-right-text">{copyrightText}</li>
+      <li>
+        <a href="https://www.quintype.com" styleName="powered-by-link" target="_blank" rel="noopener noreferrer">
+          Powered by quintype
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+const FooterBase = props => {
+  return (
+    <div styleName="wrapper">
+      <div styleName="inner-wrapper">
+        <div styleName="top-row">
+          <FooterMenuLinks footerMenuLinks={props.footerMenuLinks} />
+          <div styleName="social-media">
+            <SocialMediaIcons />
+          </div>
+        </div>
+        <div styleName="static-row">
+          <div styleName="logo">
+            <a href="/" title="Prabhat Khabar - Hindi News">
+              <img src={`${assetify(Logo)}`} alt="notification" />
+            </a>
+          </div>
+          <div>
+            <StaticFooterLinks staticFooterLinks={props.staticFooterLinks} />
+            <Copyright copyrightText={props.copyrightText} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    copyrightText: get(state, ["qt", "config", "publisher-settings", "copyright"], ""),
+    staticFooterLinks: get(state, ["qt", "data", "navigationMenu", "footerStaticMenuLinks"], []),
+    footerMenuLinks: get(state, ["qt", "data", "navigationMenu", "bigFooterLinks"], [])
+  };
+};
+
+StaticFooterLinks.propTypes = {
+  staticFooterLinks: array
+};
+
+FooterMenuLinks.propTypes = {
+  footerMenuLinks: array
+};
+
+Copyright.propTypes = {
+  copyrightText: string
+};
+
+FooterBase.propTypes = {
+  copyrightText: string,
+  staticFooterLinks: array,
+  footerMenuLinks: array
+};
+
+const Footer = connect(mapStateToProps)(FooterBase);
+
+export { Footer };
