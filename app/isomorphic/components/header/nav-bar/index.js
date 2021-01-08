44304f19a71110555db7c058bd267aa6aaadaeed
nandap4790 <nandap4790@gmail.com>
2021-01-08 15:08:14 +0530

    Add dynamic import for account modal

--- app/isomorphic/components/header/nav-bar/index.js
+++ app/isomorphic/components/header/nav-bar/index.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import get from "lodash/get";
 import { func } from "prop-types";
 
 import { NavbarSearch } from "../navbar-search";
 import { MenuItem } from "../helper-components";
 import { AppLogo } from "../app-logo";
-import AccountModal from "../../login/AccountModal";
 
 import "./styles.m.css";
 
 const NavBar = props => {
+  const AccountModal = lazy(() => import("../../login/AccountModal"));
   const [showAccountModal, setShowAccountModal] = useState(false);
   return (
     <React.Fragment>
@@ -25,7 +25,9 @@ const NavBar = props => {
         <li>
           <button onClick={() => setShowAccountModal(true)}>Login</button>
           {showAccountModal && (
-            <AccountModal onBackdropClick={() => setShowAccountModal(false)} checkForMemberUpdated={{}} />
+            <Suspense fallback={<div></div>}>
+              <AccountModal onBackdropClick={() => setShowAccountModal(false)} checkForMemberUpdated={{}} />
+            </Suspense>
           )}
           {/* checkForMemberUpdated from
           withmember of qt components library and also add onBackdropClick which is used for closing popup */}
