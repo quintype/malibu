7103104835e91d69bb73c6cda4dde6b1c2866285
nandap4790 <nandap4790@gmail.com>
2021-01-15 13:24:59 +0530

    Revert "increase benchmark"
    This reverts commit d433f31d9cd580219f80f559760612b3aa276880.

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,7 @@ const lhciConfig = {
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.6 }]
+        "categories:performance": ["error", { minScore: 0.5 }]
       }
     },
     upload: {
