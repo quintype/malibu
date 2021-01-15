a4669f011029923ab1a8834807f811e4e1bd14e0
nandap4790 <nandap4790@gmail.com>
2021-01-15 12:30:18 +0530

    Update lhci cli version

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,7 @@ const lhciConfig = {
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.9 }]
+        "categories:performance": ["error", { minScore: 0.5 }]
       }
     },
     upload: {
