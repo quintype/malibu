d433f31d9cd580219f80f559760612b3aa276880
nandap4790 <nandap4790@gmail.com>
2021-01-15 12:42:03 +0530

    increase benchmark

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,7 @@ const lhciConfig = {
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.5 }]
+        "categories:performance": ["error", { minScore: 0.6 }]
       }
     },
     upload: {
