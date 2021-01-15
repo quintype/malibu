e30b71de6b2ca96b0f061c41b4c8e61b9838aac3
nandap4790 <nandap4790@gmail.com>
2021-01-15 13:25:07 +0530

    Revert "Update lhci cli version"
    This reverts commit a4669f011029923ab1a8834807f811e4e1bd14e0.

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,7 @@ const lhciConfig = {
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.5 }]
+        "categories:performance": ["error", { minScore: 0.9 }]
       }
     },
     upload: {
