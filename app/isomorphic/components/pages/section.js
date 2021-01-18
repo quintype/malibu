153f1818a7315d364491ac6a541783cf5f8cc378
Deokumarjnu <deo.kumar.jnu@gmail.com>
2021-01-18 20:50:22 +0530

    added lazycollection to section

--- app/isomorphic/components/pages/section.js
+++ app/isomorphic/components/pages/section.js
@@ -1,15 +1,17 @@
 import React from "react";
 import PT from "prop-types";
+import get from "lodash/get";
 import { StoryGrid } from "../story-grid";
-import { replaceAllCollectionToStories } from "../helper/utils";
-
+import { collectionToStories, LazyCollection } from "@quintype/components";
+import { getCollectionTemplate } from "../get-collection-template";
 const SectionPage = props => {
-  const stories = replaceAllCollectionToStories(props.data.collection) || [];
-
+  const stories = collectionToStories(props.data.collection) || [];
+  const childCollections = get(props, ["data", "collection", "items"], []).filter(item => item.type === "collection");
   return (
     <div>
       <h1>{`Section - ${props.data.section["display-name"] || props.data.section.name}`}</h1>
       <StoryGrid stories={stories} />
+      <LazyCollection collection={{ items: childCollections }} collectionTemplates={getCollectionTemplate} />
     </div>
   );
 };
