6bcf2ce02a6e55dd390243c5e10b5e825eba6224
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-01-18 12:04:27 +0530

    implement fb login

--- app/isomorphic/components/molecules/SocialLogin/index.js
+++ app/isomorphic/components/molecules/SocialLogin/index.js
@@ -1,8 +1,7 @@
-// import React, { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 import PT from "prop-types";
 // import get from "lodash/get";
-// import { WithFacebookLogin, WithGoogleLogin /* WithTwitterLogin */ } from "@quintype/components";
+import { WithFacebookLogin, Link /* WithGoogleLogin, WithTwitterLogin */ } from "@quintype/components";
 // import { Button } from "../../atoms/button";
 import { FbIcon } from "../../atoms/icons/fb-icon";
 // import { TwitterIcon } from "../../atoms/icons/twitter-icon";
@@ -10,56 +9,92 @@ import { Google } from "../../atoms/icons/google";
 // import IntegrationData from "../../../../../config/integrations.js";
 import "./social-login.m.css";
 
+function Button({ href, children, className = "", color, flat, socialButton = false, ...props }) {
+  const classNames = `${className}`;
+  const inlineStyle = {
+    borderColor: color,
+    backgroundColor: flat && "transparent",
+    color: color
+  };
+  if (socialButton) {
+    return (
+      <a href={href} styleName={classNames} style={inlineStyle} {...props}>
+        {children}
+      </a>
+    );
+  }
+  return href ? (
+    <Link href={href} styleName={classNames} style={inlineStyle}>
+      {children}
+    </Link>
+  ) : (
+    <button styleName={classNames} style={inlineStyle} {...props}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  href: PT.string,
+  className: PT.string,
+  children: PT.any,
+  color: PT.string,
+  flat: PT.bool,
+  socialButton: PT.bool
+};
+
 export const SocialLogin = ({ checkForMemberUpdated }) => {
-  // const [error, setError] = useState("");
+  const [error, setError] = useState("");
   // const { appKeys } = IntegrationData();
   // const facebookKey = get(appKeys, ["facebookAppkey"], "");
   // const googleClientId = get(appKeys, ["googleClientId"], "");
   // const twitterKey = get(appKeys, ["twitterAppkey"], "");
 
-  // const socialLogin = (e, login) => {
-  //   e.preventDefault();
+  const socialLogin = (e, login) => {
+    e.preventDefault();
 
-  //   login()
-  //     .then(() => {
-  //       checkForMemberUpdated();
-  //       checkForMemberUpdated().then(res => {
-  //         console.log("successfully login");
-  //       });
-  //     })
-  //     .catch(error => {
-  //       console.log("error", error);
-  //       if (error === "NO_EMAIL") {
-  //         setError("The account you are using does not have an email id. Please try with another account.");
-  //       } else if (error === "NOT_LOADED") {
-  //         setError("");
-  //       } else if (error === "NOT_GRANTED") {
-  //         setError("There seems to be an error with social logins. Please do a manual email/password login.");
-  //       } else {
-  //         setError("Oops! Something went wrong. Please try again later.");
-  //       }
-  //     }); // Can also make an API call to /api/v1/members/me
-  // };
+    login()
+      .then(() => {
+        checkForMemberUpdated();
+        checkForMemberUpdated().then(res => {
+          console.log("successfully login");
+        });
+      })
+      .catch(error => {
+        console.log("error", error);
+        if (error === "NO_EMAIL") {
+          setError("The account you are using does not have an email id. Please try with another account.");
+        } else if (error === "NOT_LOADED") {
+          setError("");
+        } else if (error === "NOT_GRANTED") {
+          setError("There seems to be an error with social logins. Please do a manual email/password login.");
+        } else {
+          setError("Oops! Something went wrong. Please try again later.");
+        }
+      }); // Can also make an API call to /api/v1/members/me
+  };
 
   // const googleOnClick = (e, serverSideLoginPath) => {
   //   window.location.href = serverSideLoginPath;
   // };
+
+  console.log("error", error);
 
   return (
     <div styleName="social-login">
       <h3 styleName="title">Or login with</h3>
       <ul styleName="buttons">
         <li styleName="button">
-          {/* <WithFacebookLogin appId={facebookKey} scope="email" emailMandatory>
-          {({ login, serverSideLoginPath }) => (
-            <Button color="#3b5998" flat href={serverSideLoginPath} onClick={e => socialLogin(e, login)} socialButton> */}
-          <span styleName="icon">
-            <FbIcon color="#3b5998" width={9} height={15} />
-          </span>{" "}
-          Facebook
-          {/* </Button>
-          )}
-        </WithFacebookLogin> */}
+          <WithFacebookLogin appId={248865019954260} scope="email" emailMandatory>
+            {({ login, serverSideLoginPath }) => (
+              <Button color="#3b5998" flat href={serverSideLoginPath} onClick={e => socialLogin(e, login)} socialButton>
+                <span styleName="icon">
+                  <FbIcon color="#3b5998" width={9} height={15} />
+                </span>{" "}
+                Facebook
+              </Button>
+            )}
+          </WithFacebookLogin>
         </li>
         <li styleName="button">
           {/* <WithGoogleLogin clientId={googleClientId} scope="email" emailMandatory isBridgekeeperLogin={true}>
