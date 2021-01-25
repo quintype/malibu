d85562aee483aa3ac7a945d087cfddf6e896ba0e
Deokumarjnu <deo.kumar.jnu@gmail.com>
2021-01-25 15:04:22 +0530

    added check for author page

--- app/isomorphic/components/pages/author-page/index.js
+++ app/isomorphic/components/pages/author-page/index.js
@@ -4,7 +4,10 @@ import { object, array, shape } from "prop-types";
 import { StoryGrid } from "../../story-grid";
 
 const AuthorPage = props => {
-  const stories = props.data.stories.map(({ story }) => story);
+  const stories = props.data.stories.map(({ story }) => story) || [];
+  if (!props.data.author.name) {
+    return <h1>No author found</h1>;
+  }
   return (
     <React.Fragment>
       <h1>{`Author - ${props.data.author.name}`}</h1>
