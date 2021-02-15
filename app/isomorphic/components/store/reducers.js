70b7506502072da1f8b41f7c02284422d53ade99
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-02-15 13:24:19 +0530

    add console

--- app/isomorphic/components/store/reducers.js
+++ app/isomorphic/components/store/reducers.js
@@ -1,6 +1,7 @@
 import { MEMBER_UPDATED } from "./actions";
 
 function memberReducer(state = null, action) {
+  console.log(action, state);
   switch (action.type) {
     case MEMBER_UPDATED:
       return action.member;
