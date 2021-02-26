965106708c77903ce46a4674f159a7f45bc9e4bb
nandap4790 <nandap4790@gmail.com>
2021-02-26 08:30:31 +0530

    add min height from app scss

--- app/isomorphic/components/breaking-news-view.js
+++ app/isomorphic/components/breaking-news-view.js
@@ -48,16 +48,8 @@ export const BreakingNewsViewBase = ({ breakingNews = [], breakingNewsConfig = {
     return <div className="empty-div-margin-bottom"></div>;
   }
 
-  const breakingNewsContainerStyle = () => {
-    if (breakingNews.length > 0) {
-      return { height: "50px" };
-    } else {
-      return { height: 0 };
-    }
-  };
-
   return (
-    <div styleName="base" style={breakingNewsContainerStyle()}>
+    <div styleName="base">
       <div styleName="container">
         <div styleName="breaking-news-label">BREAKING NEWS</div>
         {renderBreakingNewsMarquee(breakingNewsItem, breakingNewsConfig)}
