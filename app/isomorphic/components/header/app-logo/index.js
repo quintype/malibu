09d438660f0e40802af39262caaa7a57d003071b
Athira MR <athiramraju001@gmail.com>
2021-03-12 17:20:43 +0530

    fix(Logo) : Update logo

--- app/isomorphic/components/header/app-logo/index.js
+++ app/isomorphic/components/header/app-logo/index.js
@@ -14,8 +14,8 @@ const AppLogo = () => (
         width="178"
         loading="lazy"
         styleName="publisher-logo"
-        srcSet="https://qtstage-01.gumlet.io/malibu/2021-03/554af9ae-cbfd-4cf7-9aed-759fc96217f1/Malibu_ullLogo.svg?auto=format%2Ccompress&format=webp&w=66&dpr=2.0"
-        src="https://qtstage-01.gumlet.io/malibu/2021-03/554af9ae-cbfd-4cf7-9aed-759fc96217f1/Malibu_ullLogo.svg?auto=format%2Ccompress&format=webp&w=66&dpr=2.0"
+        srcSet="https://qtstage-01.gumlet.io/malibu/2021-03/c49bd5b9-d324-402a-a658-cc18f6380b05/malibu_full_logo.svg?w=178"
+        data-src="https://qtstage-01.gumlet.io/malibu/2021-03/c49bd5b9-d324-402a-a658-cc18f6380b05/malibu_full_logo.svg"
         alt="Logo"
       />
     </Link>
