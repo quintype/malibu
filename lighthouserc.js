553d916b42983821993d3d5e4c7bbc6b1aa3360a
nandap4790 <nandap4790@gmail.com>
2021-03-18 16:27:18 +0530

    add number of runs

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,8 @@ const url = `https://${process.env.LH_USER}:${process.env.LH_PASSWORD}@lighthous
 const lhciConfig = {
   ci: {
     collect: {
+      method: "node",
+      numberOfRuns: 5,
       url: JSON.parse(process.env.LHCI_SITES),
       settings: {
         emulatedFormFactor: "mobile",
