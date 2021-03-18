073578d61059e71fea522040ea2355cec5c6a548
nandap4790 <nandap4790@gmail.com>
2021-03-18 18:17:46 +0530

    fix: minor refactor

--- app/server/data-loaders/home-page-data.js
+++ app/server/data-loaders/home-page-data.js
@@ -3,10 +3,12 @@ import { Collection } from "@quintype/framework/server/api-client";
 import { getStoryLimits } from "../../isomorphic/components/get-collection-template";
 
 export async function loadHomePageData(client, config, slug) {
+  const storyFields =
+    "updated-at, author-name, headline, slug, last-published-at, sections, hero-image-metadata, hero-image-s3-key, url, authors";
   const collection = await Collection.getCollectionBySlug(
     client,
     slug,
-    { "item-type": "collection" },
+    { "item-type": "collection", "story-fields": storyFields },
     { depth: 1, storyLimits: getStoryLimits() }
   );
   return {
