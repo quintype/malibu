c584cbbdb06a569f2794a1d9e73015d6b85d1b8d
nandap4790 <nandap4790@gmail.com>
2021-03-19 16:08:31 +0530

    remove fields

--- app/server/data-loaders/home-page-data.js
+++ app/server/data-loaders/home-page-data.js
@@ -3,12 +3,10 @@ import { Collection } from "@quintype/framework/server/api-client";
 import { getStoryLimits } from "../../isomorphic/components/get-collection-template";
 
 export async function loadHomePageData(client, config, slug) {
-  const storyFields =
-    "updated-at, author-name, headline, slug, last-published-at, sections, hero-image-metadata, hero-image-s3-key, url, authors";
   const collection = await Collection.getCollectionBySlug(
     client,
     slug,
-    { "item-type": "collection", "story-fields": storyFields },
+    { "item-type": "collection" },
     { depth: 1, storyLimits: getStoryLimits() }
   );
   return {
