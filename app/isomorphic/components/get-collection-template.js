9b10639e513933688cac810135dd9f5303c9d9ae
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-04-20 13:08:24 +0530

    1.update backend version to support default nested limit 2.get options for getCollectionBySlug from BK config 3.remove unused code

--- app/isomorphic/components/get-collection-template.js
+++ app/isomorphic/components/get-collection-template.js
@@ -3,12 +3,3 @@ import templates from "./collection-templates";
 export function getCollectionTemplate(designTemplate, index) {
   return templates[designTemplate] || templates.defaultTemplate;
 }
-
-const storyLimits = Object.entries(templates).reduce(
-  (acc, [key, value]) => Object.assign(acc, { [key]: value.storyLimit }),
-  {}
-);
-
-export function getStoryLimits() {
-  return storyLimits;
-}
