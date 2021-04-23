5969c098eb08380ee16420bbb081680dff00293d
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-04-23 12:22:45 +0530

    pr changes

--- app/server/data-loaders/home-page-data.js
+++ app/server/data-loaders/home-page-data.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { Collection } from "@quintype/framework/server/api-client";
 
-export async function loadHomePageData(client, config, slug, publisherAttributes) {
+export async function loadHomePageData(client, config, slug, collectionParams) {
   const collection = await Collection.getCollectionBySlug(
     client,
     slug,
     { "item-type": "collection" },
-    publisherAttributes.options
+    collectionParams
   );
   return {
     collection: collection.asJson(),
