35e784ec806410bf0f8164e9485fcb933493c5c7
Athira MR <athiramraju001@gmail.com>
2021-05-12 11:31:34 +0530

    fix(remove inline css)

--- app/isomorphic/components/layouts/header/top-bar/index.js
+++ app/isomorphic/components/layouts/header/top-bar/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./top-bar.m.css";
 
 const TopBar = () => (
-  <div styleName="main-wrapper" style={{ height: 70 }}>
+  <div styleName="main-wrapper">
     <div className="container topbar-wrapper">
       <div id="app-logo"></div>
       <div id="search-bar"></div>
