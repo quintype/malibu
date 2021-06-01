88b65534924027e75533dad1600fb2c62e2a1808
nandap4790 <nandap4790@gmail.com>
2021-06-01 13:47:58 +0530

    pick first lhci for validation

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,5 @@
 const url = `https://${process.env.LH_USER}:${process.env.LH_PASSWORD}@lighthouse-ci.staging.quinpress.com/`;
-console.log("---------------------------------------------", JSON.parse(process.env.LHCI_SITES));
-console.log("---------------------------------------------", JSON.parse(process.env.LHCI_SITES).includes("perf"));
-const lhciBenchmark = JSON.parse(process.env.LHCI_SITES).includes("perf") ? 0.9 : 0.95;
+const lhciBenchmark = JSON.parse(process.env.LHCI_SITES)[0].includes("perf") ? 0.9 : 0.95;
 const lhciConfig = {
   ci: {
     collect: {
