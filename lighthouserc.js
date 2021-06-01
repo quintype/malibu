6188d8d8d544905d397e1fc60b61ae13bbe4ced1
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-06-01 10:54:50 +0530

    update light house config with assertions and devtools

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,14 +2,31 @@ const url = `https://${process.env.LH_USER}:${process.env.LH_PASSWORD}@lighthous
 const lhciConfig = {
   ci: {
     collect: {
-      url: JSON.parse(process.env.LHCI_SITES),
+      numberOfRuns: 5,
+      additive: false, // Skips clearing of previous collect data
+      headful: false, // Run with a headful Chrome
+      url: JSON.parse(process.env.LHCI_SITES), // A URL to run Lighthouse on
       settings: {
-        emulatedFormFactor: "mobile"
+        emulatedFormFactor: "mobile",
+        throttlingMethod: "devtools"
       }
     },
     assert: {
       assertions: {
-        "categories:performance": ["error", { minScore: 0.9 }]
+        "unused-javascript": "warn",
+        "heading-order": "off", // Heading elements are not in a sequentially-descending order
+        "is-crawlable": "warn",
+        "tap-targets": "warn", // Tap targets are the areas of a web page that users on touch devices can interact with. Buttons, links, and form elements all have tap targets.
+        "uses-responsive-images": "warn",
+        "errors-in-console": "warn", // Browser errors were logged to the console
+        "uses-text-compression": "warn",
+        "uses-optimized-images": "warn",
+        "no-unload-listeners": "off",
+        "no-document-write": "warn", // Avoid `document.write()`
+        "categories:performance": ["error", { minScore: 0.95 }],
+        "image-alt": "warn",
+        "link-name": "warn",
+        "link-text": "warn"
       }
     },
     upload: {
