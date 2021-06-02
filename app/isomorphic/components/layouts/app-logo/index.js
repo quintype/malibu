def994c73585237872e38232fec4be5cfd808d80
nandap4790 <nandap4790@gmail.com>
2021-06-02 12:39:49 +0530

    fix: replace svg with png

--- app/isomorphic/components/layouts/app-logo/index.js
+++ app/isomorphic/components/layouts/app-logo/index.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link } from "@quintype/components";
+import { useSelector } from "react-redux";
+import { get } from "lodash";
 
 import "./app-logo.m.css";
 
-const AppLogo = () => (
-  <Link href="/">
-    <img
-      height="36"
-      width="178"
-      loading="lazy"
-      styleName="publisher-logo"
-      srcSet="https://qtstage-01.gumlet.io/malibu/2021-03/193b1a09-89cf-4b07-8d4f-fcf00d534577/malibu_logo_new.svg"
-      data-src="https://qtstage-01.gumlet.io/malibu/2021-03/193b1a09-89cf-4b07-8d4f-fcf00d534577/malibu_logo_new.svg"
-      alt="Logo"
-    />
-  </Link>
-);
+const AppLogo = () => {
+  const appLogo = useSelector(state =>
+    get(state, ["qt", "config", "publisher-settings", "publisher-logo", "url"], null)
+  );
+  return (
+    <Link href="/">
+      <img height="36" width="178" loading="lazy" styleName="publisher-logo" src={appLogo} alt="Logo" />
+    </Link>
+  );
+};
 
 export { AppLogo };
