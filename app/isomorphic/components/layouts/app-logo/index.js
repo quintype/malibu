a1d2526b5be86fc55d88cdebe136df699d5c11a8
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-06-03 15:29:04 +0530

    use svg instead of image for app logo

--- app/isomorphic/components/layouts/app-logo/index.js
+++ app/isomorphic/components/layouts/app-logo/index.js
@@ -1,22 +1,60 @@
 import React from "react";
 import { Link } from "@quintype/components";
-import { useSelector } from "react-redux";
-import { get } from "lodash";
 
 import "./app-logo.m.css";
 
 const AppLogo = () => {
-  const sketchesHost = useSelector(state => get(state, ["qt", "config", "sketches-host"], ""));
-  let imageUrl =
-    "https://images.assettype.com/malibu/2021-06/797284bf-9a6e-46bd-8dee-79642f02b732/malibu_logo_new.svg?w=200";
-
-  if (sketchesHost.includes("qtstage")) {
-    imageUrl = "https://qtstage-01.gumlet.io/malibu/2021-03/193b1a09-89cf-4b07-8d4f-fcf00d534577/malibu_logo_new.svg";
-  }
-
   return (
     <Link href="/">
-      <img height="36" width="178" loading="lazy" styleName="publisher-logo" src={imageUrl} alt="Logo" />
+      <svg
+        height="36"
+        width="178"
+        id="Layer_1"
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 997.64 195.19"
+      >
+        <polygon
+          className="cls-1"
+          fill="#2ac7a4"
+          points="107.91 170.72 36.93 131.06 36.93 53.16 107.91 92.83 107.91 170.72"
+        />
+        <polygon
+          className="cls-2"
+          fill="#0e8167"
+          points="144.37 170.72 215.34 131.06 215.34 53.16 144.37 92.83 144.37 170.72"
+        />
+        <polygon
+          className="cls-3"
+          stroke="#2ac7a4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2.2px"
+          opacity="0.8"
+          fill="none"
+          points="126.09 144.13 55.12 104.46 55.12 26.57 126.09 66.24 126.09 144.13"
+        />
+        <polyline
+          className="cls-3"
+          stroke="#2ac7a4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2.2px"
+          opacity="0.8"
+          fill="none"
+          points="126.09 144.13 197.16 104.67 197.16 26.78 126.18 66.44"
+        />
+        <text
+          className="cls-4"
+          fontSize="171.12px"
+          fontFamily="Montserrat-Bold, Montserrat"
+          fontWeight="700"
+          fill="#586965"
+          transform="translate(254.8 158.61)"
+        >
+          MALIBU
+        </text>
+      </svg>
     </Link>
   );
 };
