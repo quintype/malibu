cf1f557d946d60ac4cac7bade2eeb5b8ce7c49eb
Athira MR <athiramraju001@gmail.com>
2021-06-28 16:31:49 +0530

    fix(basic styling)

--- app/isomorphic/components/story-templates/blank.js
+++ app/isomorphic/components/story-templates/blank.js
@@ -1,14 +1,22 @@
 import React from "react";
 import PT from "prop-types";
+import { WithLazy, ResponsiveImage, StoryElement } from "@quintype/components";
 
-import { ResponsiveImage, StoryElement } from "@quintype/components";
+import "./blank.m.css";
 
 function StoryCard(props) {
   return (
-    <div>
-      {props.card["story-elements"].map(element => (
-        <StoryElement element={element} key={element.id} story={props.story} loadIframeOnClick />
-      ))}
+    <div styleName="story-card">
+      {props.card["story-elements"].map(element => {
+        if (element.type === "image") {
+          return (
+            <WithLazy margin="50px">
+              {() => <StoryElement element={element} key={element.id} story={props.story} loadIframeOnClick />}
+            </WithLazy>
+          );
+        }
+        return <StoryElement element={element} key={element.id} story={props.story} loadIframeOnClick />;
+      })}
     </div>
   );
 }
@@ -20,23 +28,32 @@ StoryCard.propTypes = {
 
 function BlankStoryTemplate(props) {
   return (
-    <div className="blank-story container">
-      <figure className="blank-story-image qt-image-16x9">
-        <ResponsiveImage
-          slug={props.story["hero-image-s3-key"]}
-          metadata={props.story["hero-image-metadata"]}
-          aspectRatio={[16, 9]}
-          defaultWidth={480}
-          widths={[250, 480, 640]}
-          imgParams={{ auto: ["format", "compress"] }}
-        />
-      </figure>
-      <h1>{props.story.headline}</h1>
-      <span className="blank-story-author">{props.story["author-name"]}</span>
-      {props.story.cards.map(card => (
-        <StoryCard key={card.id} card={card} story={props.story} />
-      ))}
-      <div className="space-before-next-story" style={{ minHeight: 100 }} />
+    <div className="container">
+      <div styleName="wrapper">
+        <div>
+          <WithLazy margin="20px">
+            {() => (
+              <figure className="blank-story-image" styleName="qt-image-16x9">
+                <ResponsiveImage
+                  slug={props.story["hero-image-s3-key"]}
+                  metadata={props.story["hero-image-metadata"]}
+                  aspectRatio={[16, 9]}
+                  defaultWidth={480}
+                  widths={[250, 480, 640]}
+                  sizes="( max-width: 120px ) 98%, ( max-width: 768px ) 48%, 23%"
+                  imgParams={{ auto: ["format", "compress"], fmt: "webp" }}
+                />
+              </figure>
+            )}
+          </WithLazy>
+          <h1>{props.story.headline}</h1>
+          <div styleName="author"> By {props.story["author-name"]}</div>
+          {props.story.cards.map(card => (
+            <StoryCard key={card.id} card={card} story={props.story} />
+          ))}
+        </div>
+        <div>side bar</div>
+      </div>
     </div>
   );
 }
