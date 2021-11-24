e3fc1607112b21a79f05fdbc25e1f66b39901893
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-11-24 15:21:41 +0530

    add random text on header

--- app/isomorphic/components/layouts/header/top-bar/index.js
+++ app/isomorphic/components/layouts/header/top-bar/index.js
@@ -5,6 +5,7 @@ import "./top-bar.m.css";
 const TopBar = () => (
   <div styleName="main-wrapper">
     <div className="container topbar-wrapper">
+      hee
       <div id="app-logo"></div>
       <div id="search-bar"></div>
     </div>
