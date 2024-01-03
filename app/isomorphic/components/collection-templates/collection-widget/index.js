9f7d9871ecf4f70818255221ac417ee4b902e3d2
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2024-01-03 14:28:11 +0530

    fix lint_js

--- app/isomorphic/components/collection-templates/collection-widget/index.js
+++ app/isomorphic/components/collection-templates/collection-widget/index.js
@@ -4,14 +4,12 @@ import get from "lodash/get";
 
 import "./collection-widget.m.css";
 
-const CollectionWidget = ({ collection }) => {
+export function CollectionWidget({ collection }) {
   const content = get(collection, ["associated-metadata", "content"], "");
 
   return <div styleName="wrapper" dangerouslySetInnerHTML={{ __html: content }}></div>;
-};
+}
 
 CollectionWidget.propTypes = {
   collection: object,
 };
-
-export default CollectionWidget;
