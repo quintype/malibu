503185a5295bbc2bfbecaf98910f8a65efb9672a
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2024-01-18 13:42:50 +0530

    fix collection widget rendering

--- app/isomorphic/components/collection-templates/collection-widget/index.js
+++ app/isomorphic/components/collection-templates/collection-widget/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { object } from "prop-types";
 import get from "lodash/get";
 
@@ -7,8 +7,20 @@ import "./collection-widget.m.css";
 
 export function CollectionWidget({ collection }) {
   const content = get(collection, ["associated-metadata", "content"], "");
+  const widgetRef = useRef(null);
 
-  return <div styleName="wrapper" dangerouslySetInnerHTML={{ __html: content }}></div>;
+  useEffect(() => {
+    const slotHtml = document.createRange().createContextualFragment(content);
+
+    widgetRef.current.innerHTML = "";
+    widgetRef.current.appendChild(slotHtml);
+  },[]);
+
+  return (
+    <div styleName="wrapper">
+      <div ref={widgetRef} />
+    </div>
+  );
 }
 
 CollectionWidget.propTypes = {
